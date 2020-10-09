81f6422cd9518fa0f3ec4fe0b2283a288b4ac36a
EzeCarbajo <carbajoezequiel@gmail.com>
2020-10-09 15:43:05 -0300

    Movimiento

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -16,10 +16,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalMove = Input.GetAxis("Horizontal");
+        verticalMove = Input.GetAxis("Vertical");
     }
 
     private void FixedUpdate() {
-
+        player.Move(new Vector3(horizontalMove,0,verticalMove));
     }
 }
