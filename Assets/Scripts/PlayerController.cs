847cbea9f3d85d37fd082aeca419396cf5e3989c
EzeCarbajo <carbajoezequiel@gmail.com>
2020-10-08 23:04:09 -0300

    Player Updates

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float horizontalMove;
+    private float verticalMove;
+    public CharacterController player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate() {
+
+    }
+}
