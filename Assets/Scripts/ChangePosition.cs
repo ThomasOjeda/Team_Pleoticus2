ac15b67afe453747ec1cb37ae09e3f1a74cd8b53
ThomasOjeda <thomasojeda05@gmail.com>
2020-10-13 14:45:22 -0300

    comit rehen
    commit rehen

---
+++ Assets/Scripts/ChangePosition.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangePosition : MonoBehaviour
+{
+    void OnTriggerEnter(Collider col)
+    {
+        col.transform.position = new Vector3(0, 0, 0);
+    }
+}
