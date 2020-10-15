93a60a5e21e94bfa7aa2bc9fd708bd26fa0d8164
Juan <jtripodi@alumnos.exa.unicen.edu.ar>
2020-10-15 15:05:29 -0300

    Teletransportacion

---
+++ Assets/Scripts/Teleportation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleportation : MonoBehaviour
+{
+    public Transform ubicacionTeleport;
+
+    void OnTriggerEnter(Collider other)
+    {
+        other.transform.position = ubicacionTeleport.transform.position;
+    }
+}
