dafafed13dd9eb603a72753cb3ca291bd08bd1cd
Juan <jtripodi@alumnos.exa.unicen.edu.ar>
2020-10-21 15:51:49 -0300

    Boton para abrir una pagina web

---
+++ Assets/Scripts/WebURL.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class WebURL : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AbrirWeb()
+    {
+        Application.OpenURL("www.atlassian.com/es/software/jira");
+    }
+}
