1560c963e8d2e84068ddca899240ac727586ba2e
Juan <jtripodi@alumnos.exa.unicen.edu.ar>
2020-10-22 14:57:12 -0300

    Interfaz de Asistente

---
+++ Assets/Scripts/AbrirConTecla.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbrirConTecla : MonoBehaviour
+{
+    public string tecla;
+    public GameObject canvas;
+    public GameObject jugador;
+
+    private Component movimiento;
+    private Component camara;
+
+    private Boolean activo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        activo = false;
+        movimiento = jugador.GetComponent("Character Controller");
+        camara = jugador.GetComponent("Mouse Look");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(tecla))
+        {
+            if (activo)
+            {
+                canvas.SetActive(false);
+                activo = false;
+                movimiento.enabled = true;
+                camara.SetActive(true);
+            }
+            else
+            {
+                canvas.SetActive(true);
+                activo = true;
+                movimiento.enabled = false;
+                camara.SetActive(false);
+            }
+        }
+    }
+}
