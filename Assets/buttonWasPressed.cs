8a3d48c5aa55b4edb76bd24141f3636904267e2d
ThomasOjeda <thomasojeda05@gmail.com>
2020-10-22 13:54:36 -0300

    commit rehen

---
+++ Assets/buttonWasPressed.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class buttonWasPressed : MonoBehaviour
+{
+    public void showMessage() { print("button was pressed"); }
+}
